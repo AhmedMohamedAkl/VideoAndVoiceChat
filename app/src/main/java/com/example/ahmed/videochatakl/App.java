ab53ded0c8db05ded3b62d01a3e4b8fed9aa3002
AhmedMohamedAkl <ahmedakl1991@gmail.com>
2017-08-29 13:35:38 +0200

    'message'

---
+++ app/src/main/java/com/example/ahmed/videochatakl/App.java
@@ -0,0 +1,29 @@
+package com.example.ahmed.videochatakl;
+
+import com.example.ahmed.videochatakl.util.QBResRequestExecutor;
+import com.quickblox.sample.core.CoreApp;
+
+public class App extends CoreApp {
+    private static App instance;
+    private QBResRequestExecutor qbResRequestExecutor;
+
+    public static App getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initApplication();
+    }
+
+    private void initApplication(){
+        instance = this;
+    }
+
+    public synchronized QBResRequestExecutor getQbResRequestExecutor() {
+        return qbResRequestExecutor == null
+                ? qbResRequestExecutor = new QBResRequestExecutor()
+                : qbResRequestExecutor;
+    }
+}
